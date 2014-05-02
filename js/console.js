0309fe72d06f54158539502c47e6713377c26bb2
geekbing <dhuzbb@gmail.com>
2014-05-02 22:29:56 +0800

    1
    1

---
+++ js/console.js
@@ -0,0 +1,2 @@
+console.clear();
+console.log("%c\n\t\t\t\t\t\t\t\t\tWrite  less\n\t\t\t\t\t\t\t\t\tThink  more\n\n\n","font-family: HelveticaNeue-UltraLight,Arial,sans-serif;font-size: 50px;font-weight: 300;");
